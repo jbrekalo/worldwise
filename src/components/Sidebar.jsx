e3a2166adb8d69114cfe796336423857bbfe9ff2
jbrekalo <josip.brekalo@hotmail.com>
2023-10-27 13:49:45 +0200

    app layout

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import AppFooter from "./AppFooter";
+import AppNav from "./AppNav";
+import Logo from "./Logo";
+import styles from "./Sidebar.module.css";
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+
+      <p>List of cities</p>
+
+      <AppFooter />
+    </div>
+  );
+}
+
+export default Sidebar;
