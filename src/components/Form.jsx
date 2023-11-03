1ef186b9d857086eac275dc337cd0b9a45042e36
jbrekalo <josip.brekalo@hotmail.com>
2023-11-03 13:58:43 +0100

    map jumps to selected city and reacts on click

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import styles from "./Form.module.css";
 import Button from "./Button";
 import BackButton from "./BackButton";
+import { useNavigate } from "react-router-dom";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
